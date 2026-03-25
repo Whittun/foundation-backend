aa175f89f4e7be765eb3ed1866824193e25a4006
Aleksandr <whittun@gmail.com>
2026-03-25 12:30:49 +0300

    feat: Replace the error message with a 400 error

--- src/day-rating/day-rating.service.ts
+++ src/day-rating/day-rating.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+import { BadRequestException } from '@nestjs/common';
+
 import { getYearMap } from './utils/generate-year-map';
  
 @Injectable()
 export class DayRatingService {
   findAll(year: string) {
+    const numYear = Number(year);
 
-    if (Number.isNaN(Number(year))) throw new Error('It is not valid year');
-
-    return getYearMap(Number(year));
+    if (Number.isNaN(numYear) || !Number.isInteger(numYear) || numYear < 1900 || numYear > 2100) {
+      throw new BadRequestException('It is not valid year');
+    }
+    return getYearMap(numYear);
   }
 }
