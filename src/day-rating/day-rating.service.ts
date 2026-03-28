43673be903ef577aea03c5c5e7056d516c02acf4
Aleksandr <whittun@gmail.com>
2026-03-28 12:54:45 +0300

    build: add database management to the service

--- src/day-rating/day-rating.service.ts
+++ src/day-rating/day-rating.service.ts
@@ -1,17 +1,28 @@
-import { Injectable } from '@nestjs/common';
-
-import { BadRequestException } from '@nestjs/common';
-
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { getYearMap } from './utils/generate-year-map';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { DayRating } from './day-rating.entity';
  
 @Injectable()
 export class DayRatingService {
+
+  constructor(
+    @InjectRepository(DayRating)
+    private readonly dayRatingRepository: Repository<DayRating>,
+  ) {}
+
   findAll(year: string) {
     const numYear = Number(year);
 
     if (Number.isNaN(numYear) || !Number.isInteger(numYear) || numYear < 1900 || numYear > 2100) {
       throw new BadRequestException('It is not valid year');
     }
+
+    const userId = 1;
+    const startDate = `${year}-01-01`;
+    const endDate = `${year}-12-31`;
+
     return getYearMap(numYear);
   }
 }
