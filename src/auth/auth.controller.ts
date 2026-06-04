2cc9370496d158518977bd290d50354a8d1b4506
Aleksandr <whittun@gmail.com>
2026-06-04 11:28:28 +0300

    feat: add JWT to cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -13,7 +14,16 @@ export class AuthController {
   }
 
   @Post('login')
-  login(@Body() body: LoginDto) {
-    return this.authService.login(body);
+  async login(@Body() body: LoginDto, @Res({ passthrough: true }) response: Response) {
+    const { user, accessToken } = await this.authService.login(body);
+
+    response.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: false,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return user;
   }
 }
